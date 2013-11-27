af88289ef238e74d018d27ce3f7b35e58f1d8bea
Rich Kilmer <rich.kilmer@gmail.com>
2013-11-27 08:33:51 -0500

    empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aarest::Application.config.secret_key_base = 'b7e4945c5e72073d3d327d44ad27b6276bfa33d160accf8439c32a4de0fbd03505351a7d22c1eefc77d1a559583abb2e1cff68356feefaff55892942c33f12da'
